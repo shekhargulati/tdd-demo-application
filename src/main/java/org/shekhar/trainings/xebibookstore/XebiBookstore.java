b7d5e84c53a880894fc3b5e9c4b00cf47cf0117e
Shekhar Gulati <shekhargulati84@gmail.com>
2015-05-24 02:32:21 +0530

    First test passed Yay -:)

--- src/main/java/org/shekhar/trainings/xebibookstore/XebiBookstore.java
+++ src/main/java/org/shekhar/trainings/xebibookstore/XebiBookstore.java
@@ -1,22 +1,35 @@
 package org.shekhar.trainings.xebibookstore;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class XebiBookstore {
 
-	private final String inventory;
+	private final Map<String, String> booksInventory;
 
 	public XebiBookstore(final String inventory) {
-		this.inventory = inventory;
+		this.booksInventory = toBooksInventory(inventory);
 	}
-	
+
 	public String buy(String book) {
-		return null;		
+		return booksInventory.containsKey(book) ? String.format("You have successfully bought %s", book) : null;
 	}
-	
+
+	private Map<String, String> toBooksInventory(String inventory) {
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(inventory));
+			Map<String, String> booksInventory = lines.stream().map(line -> line.split(";")).collect(Collectors.toMap(kv -> kv[0], kv -> kv[1]));
+			return booksInventory;
+		} catch (Exception e) {
+			throw new RuntimeException("Inventory not available at " + inventory);
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Xebibookstore");
 	}
-
-	
-
 
 }
--- src/test/java/org/shekhar/trainings/xebibookstore/XebibookstoreTest.java
+++ src/test/java/org/shekhar/trainings/xebibookstore/XebibookstoreTest.java
@@ -1,18 +1,25 @@
 package org.shekhar.trainings.xebibookstore;
 
-import static org.junit.Assert.assertFalse;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class XebibookstoreTest {
 
+	/**
+	 * This is the first end-to-end test which only tests how to buy a single
+	 * book at a time using a file based book inventory. First test is kept
+	 * simple yet functional to make sure it delivers some value for the end
+	 * customer.
+	 */
 	@Test
-	public void givenABookInventory_WhenUserBuysABookThatExistInInventory_ThenBookShouldBeRemovedFromTheInventory() {
-		XebiBookstore bookstore = new XebiBookstore("books.txt");
+	public void givenABookInventory_WhenUserBuysABookThatExistInInventory_ThenASuccessResponseIsShownToTheUser() {
+		XebiBookstore bookstore = new XebiBookstore("src/test/resources/books.txt");
 		String book = "Effective Java";
-		bookstore.buy(book);
-		boolean existsInInventory = bookstore.hasBookInInventory(book);
-		assertFalse(String.format("%s is sold so it does not exist in the inventory", book), existsInInventory);
+		String response = bookstore.buy(book);
+		assertThat(response, is(equalTo(String.format("You have successfully bought %s", book))));
 	}
 
 }
