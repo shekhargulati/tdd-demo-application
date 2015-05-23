b90520d90d9c2e2ba820c328bedc52ebe5672068
Shekhar Gulati <shekhargulati84@gmail.com>
2015-05-24 01:58:43 +0530

    First failing test. Buy method returns a String response instead of being void. This will help us focus on the problem at hand and keep first test clean

--- src/main/java/org/shekhar/trainings/xebibookstore/XebiBookstore.java
+++ src/main/java/org/shekhar/trainings/xebibookstore/XebiBookstore.java
@@ -2,8 +2,21 @@ package org.shekhar.trainings.xebibookstore;
 
 public class XebiBookstore {
 
+	private final String inventory;
+
+	public XebiBookstore(final String inventory) {
+		this.inventory = inventory;
+	}
+	
+	public String buy(String book) {
+		return null;		
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Xebibookstore");
 	}
+
+	
+
 
 }
