d6ace1a1d92a44d1d5c0171b7161f656e3e621dc
Shekhar Gulati <shekhargulati84@gmail.com>
2015-05-24 13:01:51 +0530

    US-1: First refactoring of our tests. Use of @Before annotation to move setup infra code to it

--- src/test/java/org/shekhar/trainings/xebibookstore/XebibookstoreTest.java
+++ src/test/java/org/shekhar/trainings/xebibookstore/XebibookstoreTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -11,28 +12,31 @@ import org.shekhar.trainings.xebibookstore.exceptions.BookNotInInventoryExceptio
 
 public class XebibookstoreTest {
 
-	/**
-	 * This is the first end-to-end test which only tests how to checkout a single
-	 * book at a time using a file based book inventory. First test is kept
-	 * simple yet functional to make sure it delivers some value for the end
-	 * customer.
-	 */
-	@Test
-	public void givenBookInventory_WhenUserCheckoutABookThatExistInInventory_ThenUserIsAskedToPayTheActualPrice() {
-		XebiBookstore bookstore = new XebiBookstore("src/test/resources/books.txt");
-		String book = "Effective Java";
-		int price = bookstore.checkout(book);
-		assertThat(price, is(equalTo(30)));
-	}
-
+	private XebiBookstore bookstore;
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
+	@Before
+	public void setup() {
+		bookstore = new XebiBookstore("src/test/resources/books.txt");
+	}
+
+	/**
+	 * This is the first end-to-end test which only tests how to checkout a
+	 * single book at a time using a file based book inventory. First test is
+	 * kept simple yet functional to make sure it delivers some value for the
+	 * end customer.
+	 */
+	@Test
+	public void givenBookInventory_WhenUserCheckoutABookThatExistInInventory_ThenUserIsAskedToPayTheActualPrice() {
+		final String book = "Effective Java";
+		final int price = bookstore.checkout(book);
+		assertThat(price, is(equalTo(30)));
+	}
+
 	@Test
 	public void givenBookInventory_WhenUserCheckoutABookThatDoesNotExistInInventory_ThenExceptionIsThrown() throws Exception {
-		XebiBookstore bookstore = new XebiBookstore("src/test/resources/books.txt");
-		String book = "Refactoring to Patterns";
-
+		final String book = "Refactoring to Patterns";
 		expectedException.expect(BookNotInInventoryException.class);
 		expectedException.expectMessage(equalTo("Sorry, 'Refactoring to Patterns' not in stock!!"));
 		bookstore.checkout(book);
